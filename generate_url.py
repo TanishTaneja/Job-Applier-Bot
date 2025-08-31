47e239d9837054f7a04c63d634110e523134cde1
tripat1803 <tripat3k2@gmail.com>
2025-08-31 13:18:43 +0530

    chore: added generate url util

---
+++ generate_url.py
@@ -0,0 +1,7 @@
+application_id="33ddd609-a882-41b2-b7b5-6c53ea277fe5"
+jobId="JOB-CA-0000000354"
+scheduleId="SCH-CA-0000003464"
+
+url = furl = f"https://hiring.amazon.ca/application/ca/?CS=true&jobId={jobId}&locale=en-CA&scheduleId={scheduleId}&ssoEnabled=1#/general-questions?CS=true&jobId={jobId}&locale=en-CA&scheduleId={scheduleId}&ssoEnabled=1&applicationId={application_id}"
+
+print(url)
